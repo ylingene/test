42540bb7c6b1759aead262069c88a65820aabe53
ylingene <ylingene@umich.edu>
2017-11-04 19:18:24 -0400

    Initial Commit

---
+++ p3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  p3
+//
+//  Created by Lingene Yang on 11/4/17.
+//  Copyright © 2017 Lingene Yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
